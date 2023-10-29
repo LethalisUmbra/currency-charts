55f6b0df7fc235c948f9b1cef62c9d56ef64b715
LethalisUmbra <anibal.cabrera2000@gmail.com>
2023-10-29 00:24:40 -0300

    FIX: Responsive

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,9 +6,9 @@ const font = Montserrat({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between pt-12 gap-5 bg-gray-900 text-white ${font.className}`}
+      className={`flex min-h-screen flex-col items-center justify-between pt-12 px-5 gap-5 bg-gray-900 text-white ${font.className}`}
     >
-      <h1 className='text-5xl font-light'>Currencies</h1>
+      <h1 className='text-4xl lg:text-5xl font-light'>Currencies</h1>
       <div className='w-4/5 h-px bg-white' />
       <Currencies />
       <footer className='bg-neutral-800 text-gray-200 text-xs p-2 text-center w-full'>
